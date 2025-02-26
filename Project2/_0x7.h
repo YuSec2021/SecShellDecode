dd6d48e8d2fbe674d8e8cdfccebb4fbbb9a5c4dd
yusec <78970427+YuSec2021@users.noreply.github.com>
2025-02-27 00:51:17 +0800

    :art:update

---
+++ Project2/_0x7.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <malloc.h>
+#include <iostream>
+#include "decode.h"
+
+void _0x7();
--- Project2/a.cpp
+++ Project2/a.cpp
@@ -3,11 +3,13 @@
 
 #include "_0x1D.h"
 #include "_0x1C.h"
+#include "_0x7.h"
 
 using namespace std;
 
 int main() {
 	//_0x1C();
-    _0x1D();
+    //_0x1D();
+    _0x7();
     return 0;
 }
